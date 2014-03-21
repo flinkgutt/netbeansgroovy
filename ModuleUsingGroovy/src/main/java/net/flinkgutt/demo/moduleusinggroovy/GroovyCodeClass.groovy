892213235258e7b525ef0edc21521b9d5485c7f0
Christian Michelsen <chris@flinkgutt.net>
2014-03-21 16:29:12 +0100

    Working test of Groovy and Java code. TopComponent calls groovy code. @Messages() annotation on the TopComponent causes build fail, thus it is disabled in the Example TopComponent.

---
+++ ModuleUsingGroovy/src/main/java/net/flinkgutt/demo/moduleusinggroovy/GroovyCodeClass.groovy
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package net.flinkgutt.demo.moduleusinggroovy
+
+/**
+ *
+ * @author Christian
+ */
+class GroovyCodeClass {
+	
+    String printStuff() {
+        println "Hello from some Groovy code!"
+        "Hello from some Groovy Code!!"
+    }
+    
+    String getNames() {
+        return "Christian, James, Mary"
+    }
+}
+
